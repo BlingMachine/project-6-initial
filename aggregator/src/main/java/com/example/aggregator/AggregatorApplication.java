4083d2986d4bcab7db746bf062f6803de6746679
BlingMachine <riceharger@gmail.com>
2023-05-11 10:09:46 -0700

    commit 2

--- aggregator/src/main/java/com/example/aggregator/AggregatorApplication.java
+++ aggregator/src/main/java/com/example/aggregator/AggregatorApplication.java
@@ -2,6 +2,10 @@ package com.example.aggregator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootApplication
 public class AggregatorApplication {
@@ -9,5 +13,4 @@ public class AggregatorApplication {
     public static void main(String[] args) {
         SpringApplication.run(AggregatorApplication.class, args);
     }
-
 }
